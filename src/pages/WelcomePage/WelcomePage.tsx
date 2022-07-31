f565aa25a08a39e73a9d1ad65532a25e50f7c1e7
dolwijit13 <dol51890@gmail.com>
2022-07-31 22:31:05 +0700

    Test: scale on scroll

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import "./WelcomePage.css";
+
 const WelcomePage = () => {
-  return <div>hello from Welcome page</div>;
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  function handleScroll() {
+    const scrollTop = window.scrollY;
+    const minHeight = 100;
+    const logoHeight = Math.max(minHeight, 400 - scrollTop);
+    setScale(logoHeight / 400);
+  }
+
+  return (
+    <div className="background">
+      <div
+        className="text"
+        style={{
+          transform: `scale(${scale})`,
+        }}
+      >
+        hello from Welcome page
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
